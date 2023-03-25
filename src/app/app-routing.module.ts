8a151274af22a9c0f4e071429dd43bdd19e1a034
fhssouza <fhssouza@gmail.com>
2023-03-25 11:07:40 -0300

    carrinho component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao
 const routes: Routes = [
   { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
   { path: "", redirectTo: "produtos", pathMatch: "full" },
+  { path: 'carrinho', loadChildren: () => import('./carrinho/carrinho.module').then(m => m.CarrinhoModule) },
   { path: "**", component: PaginaNaoEncontradaComponent},
 ];
 
