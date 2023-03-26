b2cf5966ef7c393e7e6befa0a3a23523d0c8834e
fhssouza <fhssouza@gmail.com>
2023-03-26 19:43:01 -0300

    incluindo validações nos campos do formulário

--- src/app/contato/contato.module.ts
+++ src/app/contato/contato.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ContatoRoutingModule } from './contato-routing.module';
 import { ContatoComponent } from './contato.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
 
 
 @NgModule({
@@ -11,7 +15,8 @@ import { ContatoComponent } from './contato.component';
   ],
   imports: [
     CommonModule,
-    ContatoRoutingModule
+    ContatoRoutingModule,
+    ReactiveFormsModule,
   ]
 })
 export class ContatoModule { }
